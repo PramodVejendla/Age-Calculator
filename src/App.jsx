929f275195943f06a6fef9555b9a3152e3e56f94
Pramod Vejendla <pramod.vejendla@gmail.com>
2023-12-26 14:22:11 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>hello Age </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/DateInputForm.jsx
@@ -0,0 +1,13 @@
+import "./DateInputForm.css";
+
+function DateInputForm() {
+  return (
+    <>
+      <div className="parent-container">
+        <div className="children-container"></div>
+      </div>
+    </>
+  );
+}
+
+export default DateInputForm;
